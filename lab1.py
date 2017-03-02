0b97170c1c104e3e78dac9d9ea6e49cb9e0a105e
radikalLol <radikal.xxxxx@gmail.com>
2017-03-02 10:39:57 +0400

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -1,8 +1,26 @@
+def plus(x, y):
+    print("%.2f" % (x + y))
+
+def minus(x, y):
+    print("%.2f" % (x - y))
+
+
+def division(x, y):
+    print("%.2f" % (x / y))
+
+
 x = float(input("x="))
 y = float(input("y="))
+
 print('Знак (+,-,*,/): ')
+
+
 calculations = {
-        '+': print("%.2f" % (x+y)),
-        '-': print("%.2f" % (x-y)),
-        '/': print("%.2f" % (x/y))
-    }
+        '+': plus,
+        '-': minus,
+        '/': division
+}
+
+calc_item = calculations[]
+calc_item(x, y)
+
