c715d98899eb63b710b75d2d92461746891f1d15
radikalLol <radikal.xxxxx@gmail.com>
2017-04-26 18:09:03 +0300

    Update lab3.py

--- lab3.py
+++ lab3.py
@@ -1,1 +1,19 @@
+import sys
+string = []
+
+def Poly(element):
+
+   if element == 0:
+    return 1/int(element)*3
+
+if __name__ == '__main__' :
+  string = sys.argv[1]
+  string = string.split('--poly=')
+  string = string[1].split(',')
+
+  sum = 0
+  for element in string:
+    sum += Poly(element)
+  print(string)
+  print(sum)
 
