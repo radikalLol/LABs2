b0abb18d8b4e87c6dc0e7ffcfb8216f7c22272ac
radikalLol <radikal.xxxxx@gmail.com>
2017-04-26 17:14:21 +0300

    Create number.py

---
+++ lab1OOP/number.py
@@ -0,0 +1,10 @@
+class Number:
+
+    def __init__(self, num):
+        self.__num__ = num
+
+    def getNum(self):
+        return self.__num__
+
+    def setNum(self, num):
+        self.__num__ = num
