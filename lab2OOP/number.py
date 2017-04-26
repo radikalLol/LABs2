b33c01e6243c121966abbb806e1b9eed829930aa
radikalLol <radikal.xxxxx@gmail.com>
2017-04-26 17:47:52 +0300

    Create number.py

---
+++ lab2OOP/number.py
@@ -0,0 +1,8 @@
+class Number:
+
+    def __init__(self):
+        print('Введите число из которого нужно извлечь корень: ')
+        self.__a__ = float(input())
+
+    def getNum(self):
+        return self.__a__
